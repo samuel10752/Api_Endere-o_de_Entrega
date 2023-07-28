5e7448180625ced060e8bccb8e94686d4a302110
samuel107152 <samuelcauabarbosa114@gmail.com>
2023-07-28 00:08:03 -0300

    first commit

---
+++ javascript.js
@@ -0,0 +1,441 @@
+ // Função para abrir o popup
+ function abrirPopup() {
+    const popup = document.getElementById('enderecoPopup');
+    popup.style.display = 'block'; // Exibir o popup
+}
+
+// Função para buscar o endereço com base no CEP
+function buscarEndereco(cep) {
+    const url = `http://localhost:3000/endereco/${cep}`;
+
+    // const baseUrl = ''; // Deixe em branco para usar uma URL relativa
+    // const url = `${baseUrl}/endereco/${cep}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => preencherEndereco(data))
+        .catch(error => console.log(error));
+}
+
+
+// Função para buscar o valor do frete com base no CEP
+
+function buscarFrete(cep) {
+    const url = `http://localhost:3000/frete/${cep}`;
+
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Dados processados', data);
+            exibirFrete(data);
+        })
+        .catch(error => {
+            console.error('Erro ao buscar frete', error);
+            exibirFrete({ erro: true }); // Display an error message on the page
+        });
+}
+
+
+
+function exibirFrete(data) {
+    const freteInput = document.getElementById('frete');
+    const prazoInput = document.getElementById('prazo');
+    const obsInput = document.getElementById('obs'); // Assuming you have an input field with id 'obs'
+
+    if (freteInput && prazoInput && obsInput) {
+        // Check if there's an error or if the data array is empty
+        if (data.erro || data.length === 0 || !data[0].Valor) {
+            freteInput.value = 'Frete não encontrado';
+            prazoInput.value = ''; // Clear the delivery time field in case of an error
+            obsInput.value = ''; // Clear the 'obsFim' field in case of an error
+        } else {
+            // Replace commas with dots and convert the string to a number
+            const freightValue = parseFloat(data[0].Valor.replace(',', '.'));
+            freteInput.value = 'R$ ' + freightValue.toFixed(2); // Show the value with two decimal places
+
+            // Set the delivery time
+            prazoInput.value = data[0].PrazoEntrega + ' dias'; // Append 'days' to the delivery time
+
+            // Set the 'obsFim' value
+            obsInput.value = data[0].obsFim;
+
+            // If 'obsFim' is empty, set a default message
+            if (data[0].obsFim === '') {
+                obsInput.value = 'Nenhuma observação adicional.';
+            }
+        }
+    }
+}
+
+
+
+// Função para preencher o endereço no formulário
+function preencherEndereco(endereco) {
+    const enderecoInput = document.getElementById('endereco');
+    const numeroInput = document.getElementById('numero');
+    const bairroInput = document.getElementById('bairro');
+    const cidadeInput = document.getElementById('cidade');
+    const estadoInput = document.getElementById('estado');
+    const ddInput = document.getElementById('dd');
+
+    if (enderecoInput) {
+        if (endereco.erro) {
+            enderecoInput.value = 'Endereço não encontrado';
+            numeroInput.value = '';
+            bairroInput.value = '';
+            cidadeInput.value = '';
+            estadoInput.value = '';
+            ddInput.value = '';
+        } else {
+            enderecoInput.value = endereco.logradouro || '';
+            numeroInput.value = '';
+            bairroInput.value = endereco.bairro || '';
+            cidadeInput.value = endereco.cidade || '';
+            estadoInput.value = endereco.estado || '';
+            ddInput.value = endereco.dd || '';
+        }
+    }
+}
+
+
+
+// Função para lidar com o envio do formulário
+function handleSubmit(event) {
+    event.preventDefault(); // Impedir o envio do formulário
+
+    const cep = document.getElementById('cep').value;
+    buscarEndereco(cep);
+}
+
+
+function limitarCaracteres(input, maxLength) {
+    const warningMessage = document.getElementById("warningMessage_dd");
+
+    if (input.value.length > maxLength) {
+        input.value = input.value.slice(0, maxLength); // Limita o número de caracteres
+        warningMessage.innerText = "O número de dígitos deve ser no máximo 8.";
+        input.classList.add("error-input"); // Adiciona a classe CSS para destacar o campo em vermelho
+    } else if (input.value.length === 0) {
+        warningMessage.innerText = "Você deve incluir o código do país."; // Adiciona mensagem de aviso quando a caixa estiver vazia
+        input.classList.add("error-input"); // Adiciona a classe CSS para destacar o campo em vermelho
+    } else {
+        warningMessage.innerText = ""; // Limpa a mensagem de aviso
+        input.classList.remove("error-input"); // Remove a classe CSS caso o valor seja válido
+    }
+}
+
+function validarTelefone(input) {
+    const warningMessage = document.getElementById("warningMessage_telefone");
+    const numericValue = input.value.replace(/\D/g, ""); // Remove caracteres não numéricos
+
+    if (numericValue !== input.value) {
+        input.value = numericValue; // Atualiza o valor do campo de input para conter apenas números
+        input.classList.add("error-input"); // Adiciona a classe CSS para destacar o campo em vermelho
+        warningMessage.innerText = "Insira 10 ou 11 dígitos.";
+    } else if (input.value.length === 0) {
+        warningMessage.innerText = "Insira o seu número de celular."; // Adiciona mensagem de aviso quando a caixa estiver vazia
+        input.classList.add("error-input"); // Adiciona a classe CSS para destacar o campo em vermelho
+    } else if (numericValue.length > 8) {
+        input.value = numericValue.slice(0, 9); // Limita o número de dígitos para 8
+        var numericos = input.value.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
+        var formatado = numericos.replace(/(\d{2})(\d{4,5})(\d{4})$/, "($1) $2-$3");
+        input.value = formatado;
+        input.classList.add("error-input"); // Adiciona a classe CSS para destacar o campo em vermelho
+        warningMessage.innerText = "Insira no máximo 9 dígitos.";
+    } else {
+        var numericos = input.value.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
+        var formatado = numericos.replace(/(\d{2})(\d{4,5})(\d{4})$/, "($1) $2-$3");
+        input.value = formatado;
+        input.classList.remove("error-input"); // Remove a classe CSS caso o valor seja válido
+        warningMessage.innerText = ""; // Limpa a mensagem de aviso
+    }
+}
+
+function validarLetras(input) {
+    const warningMessage = document.getElementById("warningMessage_nome");
+    const alphaRegex = /^[a-zA-Z\s]*$/; // Expressão regular para letras e espaços
+
+    if (!alphaRegex.test(input.value)) {
+        input.classList.add("error-input"); // Adiciona a classe CSS para destacar o campo em vermelho
+        warningMessage.innerText = "Favor adicionar um nome para contato.";
+    } else if (input.value.length === 0) {
+        warningMessage.innerText = "Este campo é obrigatório."; // Adiciona mensagem de aviso quando a caixa estiver vazia
+        input.classList.add("error-input"); // Adiciona a classe CSS para destacar o campo em vermelho
+    } else {
+        warningMessage.innerText = ""; // Limpa a mensagem de aviso
+        input.classList.remove("error-input"); // Remove a classe CSS caso o valor seja válido
+    }
+}
+
+
+function validarNumeros(input) {
+    var num = input.value.replace(/[^0-9]/g, ''); // Remove todos os caracteres não numéricos
+    input.value = num;
+    var numericos = input.value.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
+    var formatado = numericos.replace(/(\d{2})(\d{4,5})(\d{4})$/, "($1) $2-$3");
+    input.value = formatado;
+}
+
+function formatarCEP(input) {
+    var num = input.value.replace(/[^0-9]/g, ''); // Remove todos os caracteres não numéricos
+    input.value = num;
+    const cep = input.value.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
+    const cepFormatado = cep.replace(/(\d{5})(\d{3})/, '$1-$2'); // Formata o CEP para "37557-027"
+    input.value = cepFormatado;
+
+    const cepNumerico = cep.replace(/\D/g, ''); // Remove todos os caracteres não numéricos do CEP formatado
+    buscarEndereco(cepNumerico); // Chama a função buscarEndereco() com o valor formatado do CEP
+}
+
+function validarNumerosCPF(input) {
+    var num = input.value.replace(/[^0-9]/g, ''); // Remove todos os caracteres não numéricos
+    input.value = num;
+    const value = input.value.replace(/[^0-9]/g, '');
+    if (value.length > 11) {
+        input.value = value.slice(0, 11);
+    } else {
+        input.value = value;
+    }
+}
+function formatarCPF(input) {
+    var num = input.value.replace(/[^0-9]/g, ''); // Remove todos os caracteres não numéricos
+    input.value = num;
+    let cpf = input.value.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
+    cpf = cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4'); // Formata o CPF para "000.000.000-00"
+    input.value = cpf;
+}
+
+
+function validarEstado(input) {
+    var letras = input.value.replace(/[^a-zA-Z]/g, ""); // Remove todos os caracteres não alfabéticos
+    input.value = letras;
+    const errorDiv = document.getElementById('estado-error');
+    if (input.value === '') {
+        errorDiv.style.display = 'block';
+        errorDiv.innerText = 'Por favor, insira o estado.';
+        input.style.border = '1px solid red';  // faz a borda ficar vermelha
+    } else {
+        errorDiv.style.display = 'none';
+        input.style.border = '';  // remove a borda vermelha
+    }
+}
+
+
+function validarCidade(input) {
+    var letras = input.value.replace(/[^a-zA-Z]/g, ""); // Remove todos os caracteres não alfabéticos
+    input.value = letras;
+    const errorDiv = document.getElementById('cidade-error');
+    if (input.value === '') {
+        errorDiv.style.display = 'block';
+        errorDiv.innerText = 'Por favor, insira a cidade.';
+        input.style.border = '1px solid red';  // faz a borda ficar vermelha
+    } else {
+        errorDiv.style.display = 'none';
+        input.style.border = '';  // remove a borda vermelha
+    }
+}
+
+function validarCEP(input) {
+    var num = input.value.replace(/[^0-9]/g, ''); // Remove todos os caracteres não numéricos
+    input.value = num;
+    const errorDiv = document.getElementById('cep-error');
+    if (input.value === '') {
+        errorDiv.style.display = 'block';
+        errorDiv.innerText = 'Por favor, insira o CEP.';
+        input.style.border = '1px solid red';  // faz a borda ficar vermelha
+    } else if (input.value.length !== 8) {
+        errorDiv.style.display = 'block';
+        errorDiv.innerText = 'Por favor, insira um CEP válido.';
+        input.style.border = '1px solid red';  // faz a borda ficar vermelha
+    } else {
+        errorDiv.style.display = 'none';
+        input.style.border = '';  // remove a borda vermelha
+    }
+}
+
+
+function validarEndereco(input) {
+    var letras = input.value.replace(/[^a-zA-Z]/g, ""); // Remove todos os caracteres não alfabéticos
+    input.value = letras;
+    const errorDiv = document.getElementById('endereco-error');
+    if (input.value === '') {
+        errorDiv.style.display = 'block';
+        errorDiv.innerText = 'Por favor, insira o endereço.';
+        input.style.border = '1px solid red';  // faz a borda ficar vermelha
+    } else {
+        errorDiv.style.display = 'none';
+        input.style.border = '';  // remove a borda vermelha
+    }
+}
+
+function validarBairro(input) {
+    var letras = input.value.replace(/[^a-zA-Z]/g, ""); // Remove todos os caracteres não alfabéticos
+    input.value = letras;
+    const errorDiv = document.getElementById('bairro-error');
+    if (input.value === '') {
+        errorDiv.style.display = 'block';
+        errorDiv.innerText = 'Por favor, insira o bairro.';
+        input.style.border = '1px solid red';  // faz a borda ficar vermelha
+    } else {
+        errorDiv.style.display = 'none';
+        input.style.border = '';  // remove a borda vermelha
+    }
+}
+
+function validarApto(input) {
+    var letras = input.value.replace(/[^a-zA-Z]/g, ""); // Remove todos os caracteres não alfabéticos
+    input.value = letras;
+    const errorDiv = document.getElementById('apto-error');
+    if (input.value === '') {
+        errorDiv.style.display = 'block';
+        errorDiv.innerText = 'Por favor, insira o número do apartamento.';
+        input.style.border = '1px solid red';  // faz a borda ficar vermelha
+    } else {
+        errorDiv.style.display = 'none';
+        input.style.border = '';  // remove a borda vermelha
+    }
+}
+
+function validarNumero(input) {
+    var num = input.value.replace(/[^0-9]/g, ''); // Remove todos os caracteres não numéricos
+    input.value = num;
+    const errorDiv = document.getElementById('numero-error');
+    if (input.value === '') {
+        errorDiv.style.display = 'block';
+        errorDiv.innerText = 'Por favor, insira o número.';
+        input.style.border = '1px solid red';  // faz a borda ficar vermelha
+    } else {
+        errorDiv.style.display = 'none';
+        input.style.border = '';  // remove a borda vermelha
+    }
+}
+
+function verificarSemNumero() {
+    var checkbox = document.getElementById('semNumero');
+    var numeroInput = document.getElementById('numero');
+
+    if (checkbox.checked) {
+        numeroInput.value = 'S/N';
+        numeroInput.disabled = true;  // Desativa o campo para impedir modificações
+    } else {
+        numeroInput.value = '';
+        numeroInput.disabled = false;  // Reativa o campo para permitir modificações
+    }
+}
+
+
+// Função para abrir o modal
+function exibirEndereco() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+}
+
+// Função para fechar o modal e limpar os inputs
+function fecharPopup() {
+    var modals = document.getElementsByClassName("modal");
+    for (var i = 0; i < modals.length; i++) {
+        modals[i].style.display = "none";
+    }
+
+    var popups = document.getElementsByClassName("popup");
+    for (var j = 0; j < popups.length; j++) {
+        popups[j].style.display = "none";
+    }
+
+    // Limpar os campos de input
+    document.getElementById("nome").value = "";
+    document.getElementById("cep").value = "";
+    // Adicione aqui o código para limpar os outros campos de input
+    document.getElementById("prazo").value = "";
+    document.getElementById("frete").value = "";
+    document.getElementById("obs").value = "";
+    document.getElementById("estado").value = "";
+    document.getElementById("cidade").value = "";
+    document.getElementById("dd").value = "";
+    document.getElementById("telefone").value = "";
+    document.getElementById("endereco").value = "";
+    document.getElementById("bairro").value = "";
+    document.getElementById("apto").value = "";
+    document.getElementById("numero").value = "";
+    document.getElementById("info").value = "";
+    document.getElementById("commercial").value = "";
+    document.getElementById("residential").value = "";
+}
+
+
+// Função para salvar as informações e fechar o modal
+function salvarInformacoes() {
+    var nome = document.getElementById("nome").value;
+    var cep = document.getElementById("cep").value;
+    var prazo = document.getElementById("prazo").value;
+    var frete = document.getElementById("frete").value;
+    var obs = document.getElementById("obs").value;
+    var estado = document.getElementById("estado").value;
+    var cidade = document.getElementById("cidade").value;
+    var dd = document.getElementById("dd").value;
+    var Telefone = document.getElementById("telefone").value;
+    var endereco = document.getElementById("endereco").value;
+    var bairro = document.getElementById("bairro").value;
+    var apto = document.getElementById("apto").value;
+    var numero = document.getElementById("numero").value;
+    var info = document.getElementById("info").value;
+    var commercial = document.getElementById("commercial").value;
+    var residential = document.getElementById("residential").value;
+
+    // Verificar se todos os campos obrigatórios estão preenchidos
+    if (
+        nome === "" ||
+        cep === "" ||
+        prazo === "" ||
+        frete === "" ||
+        obs === "" ||
+        estado === "" ||
+        cidade === "" ||
+        dd === "" ||
+        Telefone === "" ||
+        endereco === "" ||
+        bairro === "" ||
+        numero === "" ||
+        commercial === "" ||
+        residential === "" 
+    ) {
+        alert("Por favor, preencha todos os campos obrigatórios.");
+        return; // Impedir o salvamento e fechamento do modal se algum campo estiver vazio
+    }
+
+    // Salvar as informações localmente usando o localStorage
+    localStorage.setItem("nome", nome);
+    localStorage.setItem("cep", cep);
+    localStorage.setItem("prazo", prazo);
+    localStorage.setItem("frete", frete);
+    localStorage.setItem("obs", obs);
+    localStorage.setItem("estado", estado);
+    localStorage.setItem("cidade", cidade);
+    localStorage.setItem("telefone", Telefone);
+    localStorage.setItem("endereço", endereco);
+    localStorage.setItem("bairro", bairro);
+    localStorage.setItem("apto", apto);
+    localStorage.setItem("numero", numero);
+    localStorage.setItem("info", info);
+    localStorage.setItem("commercial", commercial);
+    localStorage.setItem("residencial", residential);
+
+    fecharPopup();
+}
+
+
+// Evento de clique no botão "Confirmar"
+document.getElementById("enderecoPopup").onclick = exibirEndereco;
+
+// Evento de clique fora do modal para fechá-lo
+window.onclick = function (event) {
+    var modal = document.getElementById("myModal");
+    if (event.target == modal) {
+        fecharPopup();
+    }
+}
---
+++ server.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const axios = require('axios');
+const soap = require('soap');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+// Rota para buscar endereço por CEP
+app.get('/endereco/:cep', async (req, res) => {
+  try {
+    const cep = req.params.cep;
+    const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+
+    if (response.status === 200) {
+      const endereco = {
+        logradouro: response.data.logradouro,
+        bairro: response.data.bairro,
+        cidade: response.data.localidade,
+        estado: response.data.uf,
+        cep: response.data.cep,
+        dd: response.data.ddd,
+      };
+      res.json(endereco);
+    } else {
+      res.status(500).json({ message: 'Erro ao buscar endereço' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Erro ao buscar endereço', error: error });
+  }
+});
+
+// Rota para calcular o frete com base no CEP
+app.get('/frete/:cep', async (req, res) => {
+  try {
+    const cep = req.params.cep;
+    const url = 'http://ws.correios.com.br/calculador/CalcPrecoPrazo.asmx?wsdl';
+
+    const args = {
+      nCdEmpresa: '', // Seu código de empresa nos Correios, se houver
+      sDsSenha: '', // Sua senha nos Correios, se houver
+      nCdServico: '04014', // Código do serviço - 04014 é o código para SEDEX à vista
+      sCepOrigem: '37524-000', // CEP de origem
+      sCepDestino: cep, // CEP de destino
+      nVlPeso: '1', // Peso da encomenda em quilogramas
+      nCdFormato: '3', // Formato da encomenda (1 é caixa/pacote, 2 é rolo/prisma, 3 é envelope)
+      nVlComprimento: '5', // Comprimento da encomenda em centímetros
+      nVlAltura: '8', // Altura da encomenda em centímetros
+      nVlLargura: '0', // Largura da encomenda em centímetros
+      nVlDiametro: '0', // Diâmetro da encomenda em centímetros
+      sCdMaoPropria: 'n', // Indica se a encomenda será entregue em mãos ('s' ou 'n')
+      nVlValorDeclarado: '0', // Valor declarado da encomenda, para fins de seguro
+      sCdAvisoRecebimento: 'n' // Indica se a encomenda requer aviso de recebimento ('s' ou 'n')
+    };
+
+    soap.createClientAsync(url).then((client) => {
+      return client.CalcPrecoPrazoAsync(args);
+    }).then((result) => {
+      res.json(result[0].CalcPrecoPrazoResult.Servicos.cServico);
+    }).catch((err) => {
+      res.status(500).json({ message: 'Erro ao calcular o frete', error: err });
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'Erro ao calcular o frete', error: error });
+  }
+});
+
+// Ouvir na porta 3000
+app.listen(3000, () => {
+  console.log('API iniciada na porta 3000');
+});
